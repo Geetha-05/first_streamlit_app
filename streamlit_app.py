eef7a8c189fe23eb9e2e8bb8590991a81efa06bd
Geetha Maheswari <77063529+Geetha-05@users.noreply.github.com>
2023-08-07 22:44:22 +0530

    Update streamlit_app.py
    Replace Single quotes with double quotes in read_csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@
 import streamlit
 import pandas as pd
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
